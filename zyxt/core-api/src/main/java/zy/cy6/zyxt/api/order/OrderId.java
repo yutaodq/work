1a2df2df309dee025e1c5260ff1e3b5085f0f03c
yutao <zy_yutao@126.com>
2018-06-15 23:32:52 +0800

    学习不可变类

--- zyxt/core-api/src/main/java/zy/cy6/zyxt/api/order/OrderId.java
+++ zyxt/core-api/src/main/java/zy/cy6/zyxt/api/order/OrderId.java
@@ -1,6 +1,23 @@
 package zy.cy6.zyxt.api.order;
-import java.io.Serializable;
-public class OrderId implements Serializable  {
-    private String identifier;
+import org.axonframework.common.IdentifierFactory;
+
+public class OrderId {
+
+    protected final String identifier;
+
+
+    private OrderId(String identifier) {
+        if (identifier.isEmpty()) {
+
+        }
+        this.identifier = identifier;
+    }
+
+    public static OrderId OrderId(){
+        return new OrderId(IdentifierFactory.getInstance().generateIdentifier());
+    }
+    public static OrderId OrderId(String id){
+        return new OrderId(id);
+    }
 
 }
