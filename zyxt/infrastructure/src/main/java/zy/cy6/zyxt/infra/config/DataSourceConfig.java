63bafcca143e68de9b7a946184838f1aaa725d5e
yutao <zy_yutao@126.com>
2018-07-09 20:37:32 +0800

    h2 mem

--- zyxt/infrastructure/src/main/java/zy/cy6/zyxt/infra/config/DataSourceConfig.java
+++ zyxt/infrastructure/src/main/java/zy/cy6/zyxt/infra/config/DataSourceConfig.java
@@ -1,7 +1,6 @@
 package zy.cy6.zyxt.infra.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,41 +11,41 @@ import javax.sql.DataSource;
 /*多数据源的配制
  * 首先，我们必须在 application.yml 中声明两个数据源的配置，一个使用 spring.datasource ，
  * 另一个使用 spring.second-datasource ：
- * 
+ *
  * spring:
     datasource:
         #datasource 使用HikariDataSource 时，一定是 jdbcUrl不能是url
-        jdbcUrl: jdbc:h2:mem:zygl;DB_CLOSE_DELAY=-1  
+        jdbcUrl: jdbc:h2:mem:zygl;DB_CLOSE_DELAY=-1
         #server模式  不使用HikariDataSource 时，url
         url: jdbc:h2:c:/temp/zygl;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=FALSE;MODE=ORACLE;TRACE_LEVEL_FILE=3;DB_CLOSE_ON_EXIT=FALSE
         username: sa
         password:
     second-datasource:
-        jdbcUrl: jdbc:h2:mem:zyglsecond;DB_CLOSE_DELAY=-1  
+        jdbcUrl: jdbc:h2:mem:zyglsecond;DB_CLOSE_DELAY=-1
         username: sa
         password:
-
  */
 
 @Configuration
+@Slf4j
+
 public class DataSourceConfig {
-	private static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
-	
-    @Value("${spring.datasource.type}")
-    private Class<? extends DataSource> dataSourceType;
-	
-    @Bean(name = "dataSource")
+
+  @Value("${spring.datasource.type}")
+  private Class<? extends DataSource> dataSourceType;
+
+  @Bean(name = "dataSource")
 //    @Primary
 //    @Qualifier("dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-    	log.info("配制HikariDataSource数据源");
-		return DataSourceBuilder.create().type(dataSourceType).build();
-    }
-    
-    
+  @ConfigurationProperties(prefix = "spring.datasource")
+  public DataSource dataSource() {
+    log.info("配制HikariDataSource数据源");
+    return DataSourceBuilder.create().type(dataSourceType).build();
+  }
+
+
 //	第二个数据源
-    
+
 //    @Qualifier("secondDatasource")
 //    @Bean(name = "secondDatasource")
 //    @ConfigurationProperties(prefix = "spring.second-datasource")
