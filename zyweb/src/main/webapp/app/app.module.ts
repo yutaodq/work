5f7097b43cfd312e650f0a44cc82feba1dab4a1f
yutao <zy_yutao@126.com>
2018-11-18 23:22:37 +0800

    学习redux-store 加入core action

--- zyweb/src/main/webapp/app/app.module.ts
+++ zyweb/src/main/webapp/app/app.module.ts
@@ -1,28 +1,40 @@
-import './vendor.ts';
+import "./vendor.ts";
 
-import {NgModule, Injector} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {Ng2Webstorage} from 'ngx-webstorage';
-import { JhiEventManager } from 'ng-jhipster';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule, Injector } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Ng2Webstorage } from "ngx-webstorage";
+import { JhiEventManager } from "ng-jhipster";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { DBModule } from "@ngrx/db";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 // import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
-import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
-import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { ErrorHandlerInterceptor } from "./blocks/interceptor/errorhandler.interceptor";
+import { NotificationInterceptor } from "./blocks/interceptor/notification.interceptor";
 
-import {ZyxtAppRoutingModule} from './app-routing.module';
-import {ZyxtHomeModule} from './home/index';
-import {ZyxtAboutModule} from './about/index';
-import {ZyxtCoreModule} from 'app/core';
-import {ZyxtSharedModule} from 'app/shared';
-import {ZyxtMainComponent, FooterComponent, NavbarComponent, ErrorComponent, PageRibbonComponent} from './layouts';
-import {ZyxtXtwhModule} from 'app/xtwh/xtwh.module';
+import { ZyxtAppRoutingModule } from "./app-routing.module";
+import { ZyxtHomeModule } from "./home/index";
+import { ZyxtAboutModule } from "./about/index";
+import { ZyxtCoreModule } from "app/core";
+import { ZyxtSharedModule } from "app/shared";
+import {
+  ZyxtMainComponent,
+  FooterComponent,
+  NavbarComponent,
+  ErrorComponent,
+  PageRibbonComponent
+} from "./layouts";
+import { ZyxtXtwhModule } from "app/xtwh/xtwh.module";
 
 @NgModule({
   imports: [
     BrowserModule,
     ZyxtAppRoutingModule,
-    Ng2Webstorage.forRoot({prefix: 'zy', separator: '-'}),
+    Ng2Webstorage.forRoot({ prefix: "zy", separator: "-" }),
     ZyxtSharedModule,
     ZyxtCoreModule,
     ZyxtAboutModule,
@@ -30,7 +42,11 @@ import {ZyxtXtwhModule} from 'app/xtwh/xtwh.module';
     ZyxtHomeModule
   ],
   declarations: [
-    ZyxtMainComponent, FooterComponent, NavbarComponent, ErrorComponent, PageRibbonComponent
+    ZyxtMainComponent,
+    FooterComponent,
+    NavbarComponent,
+    ErrorComponent,
+    PageRibbonComponent
   ],
   providers: [
     // AuthExpiredInterceptor 的功能还没有实现
@@ -46,20 +62,15 @@ import {ZyxtXtwhModule} from 'app/xtwh/xtwh.module';
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorHandlerInterceptor,
       multi: true,
-      deps: [
-        JhiEventManager
-      ]
+      deps: [JhiEventManager]
     },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: NotificationInterceptor,
       multi: true,
-      deps: [
-        Injector
-      ]
+      deps: [Injector]
     }
   ],
   bootstrap: [ZyxtMainComponent]
 })
-export class ZyxtAppModule {
-}
+export class ZyxtAppModule {}
