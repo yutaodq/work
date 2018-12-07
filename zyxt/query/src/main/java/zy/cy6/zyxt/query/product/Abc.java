71b10660e154dbadbea1cd986e91ec89ecb557a7
yutao <zy_yutao@126.com>
2018-12-07 15:46:14 +0800

    ProductEntity.java

---
+++ zyxt/query/src/main/java/zy/cy6/zyxt/query/product/Abc.java
@@ -0,0 +1,4 @@
+package zy.cy6.zyxt.query.product;
+
+public class Abc {
+}
---
+++ zyxt/query/src/main/java/zy/cy6/zyxt/query/product/ProductEntity.java
@@ -0,0 +1,43 @@
+package zy.cy6.zyxt.query.product;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import zy.cy6.zyxt.common.domain.model.AbstractEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "product")
+@Data
+@Slf4j
+public class ProductEntity extends AbstractEntity {
+
+    @Column(name = "identifier")
+    private String identifier;
+    @Column(name = "name")
+    private String name;  //工具名称
+    @Column(name = "gg")
+    private String gg; // 规格
+    @Column(name = "xh")
+    private String xh; // 型号
+
+    public ProductEntity() {
+        log.info("新建：ProductEntity");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProductEntity)) return false;
+        AbstractEntity that = (ProductEntity) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
