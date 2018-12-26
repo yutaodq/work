456504120a015dd1b9b2715a0cd9af2347412755
yutao <zy_yutao@126.com>
2018-12-26 14:16:46 +0800

    IZyEntityService 实体服务接口

---
+++ zyweb/src/main/webapp/app/core/service/form/zy-entity.service.ts
@@ -0,0 +1,6 @@
+import { Observable } from "rxjs/index";
+import { HttpResponse } from "@angular/common/http";
+
+export interface IZyEntityService<T> {
+  create(entity: T): Observable<HttpResponse<T>>;
+}
--- zyweb/src/main/webapp/app/core/service/index.ts
+++ zyweb/src/main/webapp/app/core/service/index.ts
@@ -2,3 +2,4 @@ export * from "./analytics.service";
 export * from "./layout.service";
 export * from "./users.service";
 export * from "./form/zy-form.model";
+export * from "./form/zy-entity.service";
