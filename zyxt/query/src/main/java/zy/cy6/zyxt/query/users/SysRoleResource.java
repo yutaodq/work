33683000eea22a8ceb68e5de0306abfb78d9cad6
yutao <zy_yutao@126.com>
2019-03-12 22:51:19 +0800

    shiro/案例

---
+++ zyxt/query/src/main/java/zy/cy6/zyxt/query/users/SysRoleResource.java
@@ -0,0 +1,35 @@
+package zy.cy6.zyxt.query.users;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 角色资源关系
+ * author: 小宇宙
+ * date: 2018/4/5
+ */
+@Data
+@Entity
+public class SysRoleResource {
+
+    /**
+     * 主键id
+     */
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    /**
+     * 角色id
+     */
+    private Integer roleId;
+
+    /**
+     * 资源id
+     */
+    private Integer resourceId;
+}
---
+++ zyxt/query/src/main/java/zy/cy6/zyxt/query/users/SysUserRole.java
@@ -0,0 +1,36 @@
+package zy.cy6.zyxt.query.users;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 用户角色关系
+ * author: 小宇宙
+ * date: 2018/4/5
+ */
+@Data
+@Entity
+public class SysUserRole {
+
+    /**
+     * 主键id
+     */
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+
+    /**
+     * 用户Id
+     */
+    private Integer userId;
+
+
+    /**
+     * 角色Id
+     */
+    private Integer roleId;
+}
