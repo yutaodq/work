79e0539f6b343eac25f8b83be75d7c4737aeeecb
yutao <zy_yutao@126.com>
2019-03-19 22:11:12 +0800

    login-form.model.ts

--- zyweb/src/main/webapp/app/user/user-routing.module.ts
+++ zyweb/src/main/webapp/app/user/user-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { LoginComponent } from "./component/login.component";
+import { LoginComponent } from "./component";
 const routes: Routes = [
   {
-    path: "user",
-    children: [{ path: "login", component: LoginComponent }]
+    path: "login",
+    component: LoginComponent
   }
 ];
 
