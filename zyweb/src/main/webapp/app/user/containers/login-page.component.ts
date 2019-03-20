bf04d384d4c3c268488eb23bd37d2e0fb1224592
yutao <zy_yutao@126.com>
2019-03-20 15:57:10 +0800

    zyweb user

--- zyweb/src/main/webapp/app/user/containers/login-page.component.ts
+++ zyweb/src/main/webapp/app/user/containers/login-page.component.ts
@@ -8,6 +8,7 @@ import {
 } from "@ng-dynamic-forms/core";
 import { ActivatedRoute } from "@angular/router";
 import { LoginFormModel } from "app/user/form/login-form.model";
+import { UserService } from "app/user";
 
 @Component({
   selector: "zy-login-page",
@@ -21,7 +22,8 @@ export class LoginPageComponent implements OnInit {
   constructor(
     private _store: Store<fromLogin.State>,
     private _activatedRoute: ActivatedRoute,
-    private _formService: DynamicFormService
+    private _formService: DynamicFormService,
+    private _userService: UserService
   ) {}
 
   ngOnInit() {
@@ -42,15 +44,24 @@ export class LoginPageComponent implements OnInit {
 
   login() {
     console.log(`应用程序启动成功: login`);
+    this._userService.login(this.username(), this.password());
   }
 
   cancel() {
     console.log(`应用程序启动成功: cancel`);
   }
+  username(): string {
+    return this._formGroup.get("username").value;
+  }
+
+  password(): string {
+    return this._formGroup.get("password").value;
+  }
 
   /*
    *设置属性
    */
+
   get pageTitle(): string {
     return this._pageTitle;
   }
--- zyweb/src/main/webapp/app/user/service/user.service.ts
+++ zyweb/src/main/webapp/app/user/service/user.service.ts
@@ -1,55 +1,53 @@
-//
-//
-// File name : user.service.ts
-// Created by: Jerry Hsieh @ 2017-12-25
-//
-// Copyright (C) 2017 by Jerry Hsieh. All rights reserved
-//
-
 import { Injectable } from "@angular/core";
 
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
-import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
-import { of } from "rxjs/observable/of";
-
-import { UtilsService, TOKEN } from "app/core/service";
-// import { AppConfig } from "../../app.config";
-import { IUserEntity, Response } from "app/models";
+import { Store } from "@ngrx/store";
+import * as fromUser from "../reducers";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class UserService {
   constructor(
+    private _router: Router,
     private http: HttpClient,
-    // private appConfig: AppConfig,
-    private utils: UtilsService
+    private store: Store<fromUser.State>
   ) {}
 
-  loginServer(loginData): Observable<Response> {
-    const username = loginData.username.trim();
-    const password = loginData.password.trim();
-    return this.http.post<Response>("/users/authenticate", {
-      username: "username",
-      password: "password"
-    });
+  login(username: string, password: string) {
+    console.log("应用程序启动成功{}和{}" + username + password);
   }
-
-  // get user from server
-  getUserFromServer(): Observable<IUserEntity> {
-    if (!this.utils.isTokenExpired()) {
-      const token = this.utils.getToken();
-      return this.http
-        .post("/users/currentUser", { token: "token" })
-        .map((res: Response) => {
-          if (res.success) {
-            return { username: res.payload };
-          } else {
-            return null;
-          }
-        });
-    } else {
-      return of(null);
-    }
-  }
+  // constructor(
+  //   private http: HttpClient,
+  //   // private appConfig: AppConfig,
+  //   private utils: UtilsService
+  // ) {}
+  //
+  // loginServer(loginData): Observable<Response> {
+  //   const username = loginData.username.trim();
+  //   const password = loginData.password.trim();
+  //   return this.http.post<Response>("/users/authenticate", {
+  //     username: "username",
+  //     password: "password"
+  //   });
+  // }
+  //
+  // // get user from server
+  // getUserFromServer(): Observable<IUserEntity> {
+  //   if (!this.utils.isTokenExpired()) {
+  //     const token = this.utils.getToken();
+  //     return this.http
+  //       .post("/users/currentUser", { token: "token" })
+  //       .map((res: Response) => {
+  //         if (res.success) {
+  //           return { username: res.payload };
+  //         } else {
+  //           return null;
+  //         }
+  //       });
+  //   } else {
+  //     return of(null);
+  //   }
+  // }
 }
