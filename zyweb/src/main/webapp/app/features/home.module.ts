f3013782a398a4880af0e33973676da7a85e8a85
yutao <zy_yutao@126.com>
2019-03-25 09:07:07 +0800

    zyweb home.module.ts

--- zyweb/src/main/webapp/app/features/home.module.ts
+++ zyweb/src/main/webapp/app/features/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from "@angular/common";
 import { routing } from "./home.routing";
 import { HomeComponent } from "./home.component";
 
+const IMPORTS_MODULES = [CommonModule, routing];
+const COMPONENT = [HomeComponent];
 @NgModule({
-  imports: [CommonModule, routing],
-  declarations: [HomeComponent],
+  imports: [...IMPORTS_MODULES],
+  declarations: [...COMPONENT],
   exports: []
 })
 export class HomeModule {}
--- zyweb/src/main/webapp/app/features/home.routing.ts
+++ zyweb/src/main/webapp/app/features/home.routing.ts
@@ -1,8 +1,5 @@
 import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ContactComponent } from "./contact/contact.component";
-import { ForgotComponent } from "./forgot/forgot.component";
-import { NotfoundComponent } from "./errors/notfound.component";
 
 export const routes: Routes = [
   {
@@ -13,42 +10,6 @@ export const routes: Routes = [
   {
     path: "main",
     loadChildren: "./main/main.module#MainModule"
-  },
-  {
-    path: "login",
-    loadChildren: "./login/login.module#LoginModule"
-  },
-  {
-    path: "register",
-    loadChildren: "./register/register.module#RegisterModule"
-  },
-  {
-    path: "forgot-password",
-    component: ForgotComponent,
-    data: { pageTitle: "Forget Password" }
-  },
-  {
-    path: "locked",
-    loadChildren: "./locked/locked.module#LockedModule"
-  },
-  {
-    path: "dashboard",
-    loadChildren: "./dashboard/dashboard.module#DashboardModule",
-    data: { pageTitle: "Dashboard" }
-  },
-  {
-    path: "order",
-    loadChildren: "./orders/orders.module#OrdersModule",
-    data: { pageTitle: "User Orders" }
-  },
-  {
-    path: "contact",
-    component: ContactComponent
-  },
-  {
-    path: "notfound",
-    component: NotfoundComponent,
-    data: { title: "404 Error" }
   }
 ];
 
